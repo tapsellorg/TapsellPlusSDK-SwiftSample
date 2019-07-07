df1fe94b05c31ed15b8152c8119eb4c0033922bc
Mahdi Toori <mtoori96@gmail.com>
2019-07-08 01:34:53 +0430

    Initial Commit

---
+++ TapsellPlusSDK-SwiftSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TapsellPlusSDK-SwiftSample
+//
+//  Created by Mahdi Toori on 4/17/1398 AP.
+//  Copyright © 1398 Tapsell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
